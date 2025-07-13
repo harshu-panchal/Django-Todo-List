c1bb12d0ee7334cbf0113f8c56410c574f4b600a
harshu-panchal <harshvardhanpanc145@gmail.com>
2025-07-13 13:24:18 +0530

    second commit

--- todo_main/views.py
+++ todo_main/views.py
@@ -2,9 +2,9 @@
 from django.shortcuts import render
 from todo.models import Task
 
-
+ 
 def home(request):
-    tasks = Task.objects.filter(isCompleted = False)
+    tasks = Task.objects.filter(isCompleted = False).order_by('-updatedAt')
     Completed = Task.objects.filter(isCompleted = True)
     context = {
         'tasks' : tasks,
