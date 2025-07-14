810da53072d160821ec917cca5a046834ed96baf
harshu-panchal <harshvardhanpanc145@gmail.com>
2025-07-14 11:48:13 +0530

    final commit

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Task
 
 
@@ -8,3 +8,33 @@ def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
     return redirect('home')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.isCompleted = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.isCompleted = False
+    task.save()
+    return redirect('home')
+
+def removeTask(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
+
+def editTask(request, pk):
+    getTask = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        newTask = request.POST['task']
+        getTask.task = newTask
+        getTask.save()
+        return redirect('home')
+    else:
+        context = {
+            "getTask" : getTask,
+        }
+        return render(request, 'editTask.html', context)
